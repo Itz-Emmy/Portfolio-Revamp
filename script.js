22b3ceb2c4b57b522803b10e60fbdfd382c2815a
Emmanuel Agbonyeme <nereus4luv@yahoo.com>
2023-09-27 13:36:43 +0100

    dark mode functinality

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const openHamburger = document.querySelector(".open");
 const closeHamburger = document.querySelector(".close");
+const openHamburgerLight = document.getElementById("dark-menu-open");
+const closeHamburgerDark = document.getElementById("dark-menu-close");
 const linksContainer = document.querySelector(".links-container");
 const links = document.querySelector(".links");
+
+openHamburgerLight.style.display = "none";
 
 openHamburger.addEventListener("click", () => {
   openHamburger.style.display = "none";
@@ -13,6 +17,19 @@ openHamburger.addEventListener("click", () => {
 closeHamburger.addEventListener("click", () => {
   closeHamburger.style.display = "none";
   openHamburger.style.display = "block";
+
+  linksContainer.style.display = `none`;
+});
+openHamburgerLight.addEventListener("click", () => {
+  openHamburgerLight.style.display = "none";
+  closeHamburgerDark.style.display = "block";
+  linksContainer.style.display = `block`;
+  linksContainer.style.width = "100vw";
+});
+
+closeHamburgerDark.addEventListener("click", () => {
+  closeHamburgerDark.style.display = "none";
+  openHamburgerLight.style.display = "block";
 
   linksContainer.style.display = `none`;
 });
@@ -75,4 +92,141 @@ scrollLinks.forEach((link) => {
     closeHamburger.style.display = "none";
     openHamburger.style.display = "block";
   });
+});
+
+const footerLogoDark = document.querySelectorAll(".dark");
+const footerLogoLight = document.querySelectorAll(".light");
+
+footerLogoDark.forEach((e) => {
+  e.style.display = "none";
+});
+
+// Get the toggle switch and elements that need dark mode adjustments
+const toggleSwitch = document.getElementById("toggle");
+const sections = document.getElementsByTagName("section");
+const header = document.getElementById("home");
+const nav = document.getElementById("nav");
+const body = document.body;
+const logoDark = document.querySelector(".dark-logo");
+const logoLight = document.querySelector(".light-logo");
+const footer = document.getElementById("contact");
+const footerColumns = document.querySelector(".footer-columns");
+const footerBottom = document.querySelector(".footer-bottom");
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+console.log(prefersDarkScheme);
+
+const toggleDarkMode = () => {
+  if (toggleSwitch.checked) {
+    // Enable dark mode
+
+    const addClassToSectionAndChildren = () => {
+      openHamburgerLight.style.display = "block";
+      openHamburger.style.display = "none";
+      for (const section of sections) {
+        section.classList.add("dark-mode");
+        header.classList.add("dark-mode");
+        nav.style.background = "#333333";
+        body.classList.add("dark-mode");
+        footerColumns.classList.add("dark-mode");
+        footerBottom.classList.add("dark-mode");
+        footer.classList.add("dark-mode");
+        logoLight.style.display = "none";
+        logoDark.style.display = "block";
+
+        addClassToElementAndChildren(section, "dark-mode");
+        footerLogoDark.forEach((e) => {
+          e.style.display = "block";
+        });
+        footerLogoLight.forEach((e) => {
+          e.style.display = "none";
+        });
+      }
+    };
+
+    const addClassToElementAndChildren = (element, className) => {
+      element.classList.add(className);
+
+      const childElements = element.querySelectorAll("*");
+      childElements.forEach((child) => {
+        child.classList.add(className);
+      });
+    };
+    addClassToSectionAndChildren();
+
+    localStorage.setItem("darkMode", "enabled");
+  } else {
+    // Disable dark mode
+    const removeClassFromSectionAndChildren = () => {
+      openHamburgerLight.style.display = "none";
+      openHamburger.style.display = "block";
+      for (const section of sections) {
+        section.classList.remove("dark-mode");
+        header.classList.remove("dark-mode");
+        nav.style.background = "#ffffff";
+        body.classList.remove("dark-mode");
+        footerColumns.classList.remove("dark-mode");
+        footerBottom.classList.remove("dark-mode");
+        footer.classList.remove("dark-mode");
+        logoLight.style.display = "block";
+        logoDark.style.display = "none";
+
+        removeClassFromElementAndChildren(section, "dark-mode");
+        footerLogoDark.forEach((e) => {
+          e.style.display = "none";
+        });
+        footerLogoLight.forEach((e) => {
+          e.style.display = "block";
+        });
+      }
+    };
+    const removeClassFromElementAndChildren = (element, className) => {
+      element.classList.remove(className);
+      const childElements = element.querySelectorAll("*");
+      childElements.forEach((child) => {
+        child.classList.remove(className);
+      });
+    };
+    removeClassFromSectionAndChildren();
+  }
+  if (toggleSwitch.checked) {
+    localStorage.setItem("darkMode", "enabled");
+  } else {
+    localStorage.setItem("darkMode", "disabled");
+  }
+};
+
+// Function to check the initial dark mode setting
+const checkDarkMode = () => {
+  const darkModeSetting = localStorage.getItem("darkMode");
+
+  // Check if the user's device is in dark mode and enable dark mode accordingly
+  if (prefersDarkScheme.matches) {
+    toggleSwitch.checked = true;
+    toggleDarkMode();
+  } else if (darkModeSetting === "enabled") {
+    toggleSwitch.checked = true;
+    toggleDarkMode();
+  } else {
+    toggleSwitch.checked = false;
+    removeClassFromSectionAndChildren();
+  }
+};
+
+// Event listener for toggle switch
+toggleSwitch.addEventListener("change", toggleDarkMode);
+
+// Check initial dark mode setting
+checkDarkMode();
+
+prefersDarkScheme.addEventListener("change", (e) => {
+  if (e.matches) {
+    // User switched to dark mode
+    toggleSwitch.checked = true;
+    toggleDarkMode();
+  } else {
+    // User switched to light mode
+    toggleSwitch.checked = false;
+    removeClassFromSectionAndChildren();
+  }
 });
